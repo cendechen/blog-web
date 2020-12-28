8a0278c391868762e07da94db3bb4a3d5688d5e2
cendechen <307142162@qq.com>
2020-12-29 00:44:02 +0800

    feat: 更新数据

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const { override, addLessLoader } = require('customize-cra')
+const { addPostcssPlugins } = require('./addPostcssPlugins')
 
 module.exports = override(
   addLessLoader({
@@ -8,5 +9,13 @@ module.exports = override(
         '@base-color': '#f44336'
       }
     }
-  })
+  }),
+  addPostcssPlugins([
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'), // or require('postcss-nesting')
+    require('postcss-custom-properties'),
+    require('postcss-preset-env')({ stage: 1 }),
+    require('autoprefixer')
+  ])
 )
