2d0018053d42897fed91ac591dec7bc4d5ad3ce6
cendechen <307142162@qq.com>
2021-12-12 11:16:25 +0800

    feat: 修复eslint错误

--- src/view/main/index.js
+++ src/view/main/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Sider } from './components/sider'
 import { AricleList } from './components/aricle'
 import './index.less'
-import { useObervableApi } from '../../services'
+// import { useObervableApi } from '../../services'
 
 export const Main = () => {
-  const data = useObervableApi({
-    url: '/api/getList'
-  })
+  // const data = useObervableApi({
+  //   url: '/api/getList'
+  // })
   return  <div className="flex mx-auto container py-4">
     <Sider />
     <AricleList />
